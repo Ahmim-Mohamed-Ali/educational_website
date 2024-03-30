9aae6b01e105503646bbbdc1522c0e0e38ab6505
Mohamed-Ali <ecomsbranding@gmail.com>
2024-03-30 03:18:31 +0100

    Commit2: Chat Entre Etudiant Achevé et Mise En Place des tables pour lier un cours avec un prof et un etudiant avec un prof pour afficher les bons cours aux bon etudiants

---
+++ Vue/cours_admin.php
@@ -0,0 +1,13 @@
+<?php foreach ($cours as  $cour): ?>
+    <div class="cours ">
+        <h4> <?= $cour['titre'] ?></h4>
+        <p>Description: <?= $cour['description'] ?></p>
+        <p>Url Vidéo: <?= $cour['url'] ?></p>
+        <img src="../img/cours_default_image.webp" alt="" srcset="" class="image_cours">
+        <button><a href="<?= $cour['chemin'] ?>" target="_blank">Voir le Cours</a></button>
+        <form action="" method="post">
+            <input type="hidden" name="id" value=<?=$cour['id'] ?>>
+        <button type="submit" name="btn-delete" >Supprimer</button>
+        </form>
+    </div>
+<?php endforeach; ?>
